113553bc6b1d9332d92bcc6aa18d98c95908603e
Richard Williams <richard.williams2@manchester.ac.uk>
2017-10-11 10:51:29 +0100

    Added local storage

--- js/main.js
+++ js/main.js
@@ -1,27 +1,60 @@
+let isReady = false;
+let isData = false;
+let data = {};
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function () {
+    navigator.serviceWorker.register('/sw.js').then(function (registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function (err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
+
+localforage.getItem('d').then(function (val) {
+  isData = true;
+  console.log(val);
+  if (val) data = val;
+  if (isReady && isData) init();
+});
+
+const updateLocal = () => {
+  localforage.setItem('d', data).catch(err => console.log(err));
+};
 
 const flip = function ($icon, $other, $result, $wrapper) {
   const isStar = $icon.text().indexOf('star') > -1;
+  const dateString = $wrapper.data('date');
 
   if (!isStar) {
     $result.addClass('red');
     $result.find('i').text('sentiment_very_dissatisfied');
     $wrapper.addClass('flip');
+    data[dateString] = false;
   } else {
     $result.addClass('yellow');
     $result.find('i').text('star');
     $wrapper.addClass('flip');
+    data[dateString] = true;
   }
+  updateLocal();
 };
 
-const unflip = function ($icon, $othe, $result, $wrapper) {
+const unflip = function ($result, $wrapper) {
   $wrapper.removeClass('flip').off('click');
   $result.removeClass('yellow red');
+  const dateString = $wrapper.data('date');
+  delete data[dateString];
 
+  updateLocal();
   wireUpCards();
 };
 
 const wireUpCards = function () {
-  $('.material-icons').off('click').on('click', function (e) {
+  $('.material-icons.sad,.material-icons.star').off('click').on('click', function (e) {
     e.stopPropagation();
     $icon = $(this);
     $other = $($icon.siblings()[1]);
@@ -33,26 +66,46 @@ const wireUpCards = function () {
     flip($icon, $other, $result, $wrapper);
 
     $wrapper.on('click', function () {
-      unflip($icon, $other, $result, $wrapper);
+      unflip($result, $wrapper);
     });
+  });
+  $('.card-wrapper.flip').off('click').on('click', function (e) {
+    e.stopPropagation();
+
+    const $wrapper = $(this);
+    const $result = $wrapper.find('.result');
+    $wrapper.find('.material-icons').off('click');
+
+    unflip($result, $wrapper);
   });
 };
 
-$(document).ready(function () {
+const init = () => {
   var dayTemplate = $('#dayTemplate').html();
   Mustache.parse(dayTemplate);
   var today = new Date();
-  for (var i = 0; i < 20; i++) {
+  var startDate = new Date(2017, 9, 9);
+  for (var i = 0; i < 2000; i++) {
     var row = $('<div class="row"></div>');
     for (var j = 0; j < 7; j++) {
-      var dayRendered = Mustache.render(dayTemplate, { date: today.toString().substr(0, 10) });
-      today.setDate(today.getDate() + 1);
+      var displayDate = startDate.toString().substr(0, 10);
+      var dayRendered = Mustache.render(dayTemplate, {
+        isStar: displayDate in data ? data[displayDate] : false,
+        isSad: displayDate in data ? !data[displayDate] : false,
+        date: displayDate,
+        isFuture: startDate > today
+      });
+      startDate.setDate(startDate.getDate() + 1);
       row.append($(dayRendered));
     }
-    $('.o' + (Math.floor(i / 5) + 1)).append(row);
+    $('#fullpage').append(row);
+    if (i > 3 && startDate > today) break;
   }
 
   wireUpCards();
+};
 
-  //$('#fullpage').fullpage();
+$(document).ready(function () {
+  isReady = true;
+  if (isReady && isData) init();
 });
---
+++ sw.js
@@ -0,0 +1,84 @@
+// Change this to force update v1.0.4
+var CACHE = 'cache-update-and-refresh';
+
+var cachedLocalItems = [
+  '/css/main.css',
+  'js/main.js',
+  'index.html'
+];
+
+var cachedRemoteItems = [
+  'https://fonts.googleapis.com/icon?family=Material+Icons',
+  'https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css',
+  'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/mustache.js/2.3.0/mustache.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/localforage/1.5.2/localforage.min.js'
+];
+
+var allCachedItems = cachedLocalItems.concat(cachedRemoteItems);
+
+self.addEventListener('install', function (e) {
+  // Add the following assets on install
+  console.log('sw.js -> Install');
+  e.waitUntil(caches.open(CACHE).then(function (cache) {
+    cache.addAll(allCachedItems)
+  }));
+});
+
+self.addEventListener('fetch', function (evt) {
+  console.log('sw.js -> Fetch ' +evt.request.url);
+  // On fetch immediately return from cache if it is something
+  // we currently cache
+  if(cachedLocalItems.indexOf(evt.request.url.replace(/^https?:\/\/[^/]+\//,"/")) > -1 ||
+      cachedRemoteItems.indexOf(evt.request.url)>-1) {
+
+    evt.respondWith(fromCache(evt.request));
+
+    // Then try to get a network version
+    evt.waitUntil(update(evt.request).then(refresh));
+  } else {
+    evt.respondWith(fetch(evt.request).catch(function () {
+      console.log("Error caught - offline.html returned");
+      return caches.match('/offline.html');
+    }));
+  }
+});
+
+function fromCache(request) {
+  console.log('sw.js -> Fetch ' + request.url + ' from cache.');
+  return caches.open(CACHE).then(function (cache) {
+    return cache.match(request);
+  });
+}
+
+function update(request) {
+  return caches.open(CACHE).then(function (cache) {
+    return fetch(request).then(function (response) {
+      return cache.put(request, response.clone()).then(function () {
+        return response;
+      });
+    });
+  }).catch(function(){
+    return null;
+  });
+}
+
+function refresh(response) {
+  if(!response) return;
+  console.log('sw.js -> Response url ' + response.url);
+  console.log('sw.js -> Response headers ' + Array.from(response.headers.keys()));
+  return self.clients.matchAll().then(function (clients) {
+    clients.forEach(function (client) {
+ 
+      var message = {
+        type: 'refresh',
+        url: response.url,
+ 
+        eTag: response.headers.get('ETag')
+      };
+ 
+      client.postMessage(JSON.stringify(message));
+    });
+  });
+}
